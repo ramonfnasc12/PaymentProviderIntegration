b9ccdec3179ba24e814ff8f4b41e68d96a1cef63
Ramon Nascimento <usuario@C02GN95LQ05D.local>
2023-03-13 18:44:45 -0300

    Adding checkout files

---
+++ react/pages.js
@@ -0,0 +1,18 @@
+import React, { Component }  from 'react'
+class CheckoutListener extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    console.log(navigator.userAgent)
+  }
+
+  render() {
+    return (
+      <></>
+    )
+  }
+}
+
+export default CheckoutListener
